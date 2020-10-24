6580ce24e4bad119b7c77a0a5cb3252bef754f4f
Adrian Nikiel <hunter86@onet.eu>
2020-10-24 23:17:34 +0200

    changed DB connection data to heroku postgresql

--- src/main/java/pl/anikiel/sqlwyzwanie/repository/PSQLConnection.java
+++ src/main/java/pl/anikiel/sqlwyzwanie/repository/PSQLConnection.java
@@ -6,11 +6,11 @@ import java.util.Properties;
 public class PSQLConnection {
 
     public static Connection getConnecton() throws SQLException {
-        String url = "jdbc:postgresql://127.0.0.1:5432/sqlwyzwanie";
+        String url = "jdbc:postgresql://ec2-54-228-250-82.eu-west-1.compute.amazonaws.com/d1ocao8gribcc7?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
         Properties props = new Properties();
-        props.setProperty("user", "adrian");
-        props.setProperty("password", "passw0rd");
-        props.setProperty("ssl", "true");
+        props.setProperty("user", "lzkdykqedceymd");
+        props.setProperty("password", "ffd7738e52ac7763dcd23f3703ece771385557798585136e088012aa0019adb1");
+        props.setProperty("ssl", "false");
 
         return DriverManager.getConnection(url, props);
     }
